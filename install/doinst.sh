6c145cb3dc7441ab524b9de2e115459bb857782b
Tasha S <tasha@eviltasha.com>
2015-09-07 18:54:25 -0700

    removed dirty files

--- install/doinst.sh
+++
@@ -1,4 +0,0 @@
-( cd usr/share/man/man1 ; rm -rf sendmail.1.gz )
-( cd usr/share/man/man1 ; ln -sf msmtp.1.gz sendmail.1.gz )
-( cd usr/sbin ; rm -rf sendmail )
-( cd usr/sbin ; ln -sf ../bin/msmtp sendmail )
